22d2d98f35b26482e325095cbac5995afe3f38ab
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2024-09-09 15:21:42 +0400

    Theory

--- DevTdd.Course.UnitTests/RomanNumbersShould.cs
+++ DevTdd.Course.UnitTests/RomanNumbersShould.cs
@@ -4,12 +4,13 @@ namespace DevTdd.Course.UnitTests
 {
     public class RomanNumbersShould
     {
-        [Fact]
-        public void ReturnOne()
+        [Theory]
+        [InlineData(1, "I")]
+        public void ReturnOne(int number, string expected)
         {
-            var romanNumber = RomanNumber.Convert(1);
+            var romanNumber = RomanNumber.Convert(number);
 
-            romanNumber.Should().Be("I");
+            romanNumber.Should().Be(expected);
         }
     }
 
