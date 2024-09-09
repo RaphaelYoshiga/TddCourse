ca96c572f15eebf69f45df9ae7d340d35c810581
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2024-09-09 15:20:12 +0400

    Initial commit

---
+++ DevTdd.Course.UnitTests/RomanNumbersShould.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+
+namespace DevTdd.Course.UnitTests
+{
+    public class RomanNumbersShould
+    {
+        [Fact]
+        public void ReturnOne()
+        {
+            var romanNumber = RomanNumber.Convert(1);
+
+            romanNumber.Should().Be("I");
+        }
+    }
+
+    public class RomanNumber
+    {
+        public static string Convert(int i)
+        {
+            return "I";
+        }
+    }
+}
